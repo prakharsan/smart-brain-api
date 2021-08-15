84cd6391c15479425643d91a235aba2b282f5989
prakharsan <prakhar3099@gmail.com>
2021-08-15 21:17:24 +0530

    connectionString

--- server.js
+++ server.js
@@ -11,9 +11,11 @@ const image = require('./controllers/image');
 const db =knex({
   client: 'pg',
   connection: {
-    connectionString : process.env.DATABASE_URL,
-    ssl : true,
-  }
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+        rejectUnauthorized: false,
+        }
+    }
 });
 
 // postgres.select('*').from('users').then( data =>{
